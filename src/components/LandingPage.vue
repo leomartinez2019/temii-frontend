4e2043246551704c779050c7541c28f8537c542b
leomartinez2019 <lowenhard@yahoo.com>
2020-03-05 11:14:11 -0500

    agregar página para no loggeados

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="landing-page">
+    <h1 class="logo">Temii</h1>
+    <p>
+      Temii es una aplicación para ayudar a la comunidad de Python 
+      Barranquilla a generar temas para su meetup.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DummyCompo',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+
+</script>
+
+<style>
+
+.logo {
+  font-family: Pacifico;
+  color: #00a7e1;
+}
+
+</style>
+
